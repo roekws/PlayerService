932db256133ba0722eb1eb7af21cc9587a8f7918
roekws <roekws@mail.ru>
2025-06-13 17:23:37 +0500

    Fixed namespace

--- src/Players.API/Infrastructure/Authentication/AuthHandler.cs
+++ src/Players.API/Infrastructure/Authentication/AuthHandler.cs
@@ -3,7 +3,6 @@ using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 using Players.API.Infrastructure.Authorization.Claims;
-using Players.API.Infrastructure.Errors;
 using Scalar.AspNetCore;
 
 namespace Players.API.Infrastructure.Authentication;
